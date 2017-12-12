23c3694fd565cfda192f0a31662a4d6fb538b7e1
Kenenth Kristensen <kehk@Kenenths-MacBook-Pro.local>
2017-12-12 11:51:04 +0100

    Add computed func. Add comments

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>{{header}}</h1>
-        <p>By {{author.title}} {{author.name}}</p>
+        <p>By {{authorLabel}}</p>
 
         <button v-on:click="fire">F I R E</button>
 
@@ -11,13 +11,18 @@
 <script>
     export default {
         name: 'test',
-        data () {
+        data () {  //  Fields availeble in component
             return {
                 header : 'Sometinh component',
                 author : {name : 'kenkris', title : 'dev.'}
             }
         },
-        methods : {
+        computed : {  //  Avoid as much logic as possible in template. So composite strings etc are done here.
+            authorLabel : function(){
+                return this.author.title + ' ' + this.author.name;
+            }
+        },
+        methods : {  //  Methods available to component
             fire : function(){
                 console.log('fire!!');
             }
